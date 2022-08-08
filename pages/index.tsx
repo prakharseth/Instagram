dfe03b4b02d2093eff34fd18f5b467dd3856750a
prakharseth <sethji2003@gmail.com>
2022-08-08 18:31:03 +0530

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,91 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head"
+import { useState } from 'react';
 
 export default function index() {
+
+  const [todos, setTodos] = useState([]);
   return (
+
     <div>
       <Head>
-        <title>Instagram</title>
-        <link rel="icon" href="/insta.png" />
-        {/* <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet"></link> */}
+        <title>Todo App</title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700&display=swap" rel="stylesheet"></link>
       </Head>
-      <main className='flex flex-col justify-center items-center mt-20'>
-        <div className='border rounded-sm flex flex-col justify-center items-center w-[350px] h-[393px]'>
-          <div className='w-[192px] m-8'>
-            <img src="/instagram.png" alt="" />
-          </div>
+      <div className="flex mt-32 justify-center h-screen font-nunito">
+        <div className="flex flex-col items-center">
 
-          <div className='flex flex-col gap-2'>
-            <div className='w-[268px] h-[38px] border pl-2 rounded-sm'>
-              <input type="text" className='placeholder:text-xs w-[258px] h-[36px] outline-none' placeholder='Phone number, username, or email' />
-            </div>
-            <div className='w-[268px] h-[38px] border pl-2 rounded-sm'>
-              <input type="password" className='placeholder:text-xs w-[258px] h-[36px] outline-none' placeholder='Password' />
-            </div>
-          </div>
+          <div className="text-3xl font-bold text-slate-700">Welcome to My Todo App</div>
 
-          <div className='m-4 w-[268px] h-[30px] flex items-center {bg-[#b2dffc]} rounded justify-center text-white font-medium bg-[#0095f6] cursor-pointer'>Log In
-          </div>
+          <input className="border-[1.4px] rounded-sm h-8 mt-10 pl-2 w-80 border-slate-600"
+            placeholder="Enter Your Task" type="text" />
 
-          <div className='flex justify-between items-center w-[268px]'>
-            <div className='border-t-[1.5px] border-[#dbdbdb] w-[100px]'></div>
-            <div className='text-[#8e8e8e] text-sm font-medium'>OR</div>
-            <div className='border-t-[1.5px] border-[#dbdbdb] w-[100px]'></div>
-          </div>
+          <button className="mt-4 border p-2 rounded-sm text-center bg-slate-700 text-white">Submit</button>
 
-          <div className='flex justify-center items-center gap-2 mt-6'>
-            <div className='w-4'>
-              <img src="fb.png" alt="fb" />
-            </div>
-            <a href='https://fb.com' className='text-[#385185] text-sm font-medium'>Login in with Facebook</a>
-          </div>
-
-          <a href='https://www.instagram.com/accounts/password/reset/' className='flex items-center  justify-center text-xs text-[#00376b] font-normal my-4'>Forgot Password?</a>
+          <div className="text-2xl font-semibold text-gray-600 mt-4 underline">List of Tasks</div>
+          <p className="text-lg mt-3">Here My Tasks</p>
         </div>
-
-        <div className='w-[350px] h-[63px] border rounded-sm flex items-center justify-center mt-2'>
-          <div className='text-sm pr-1'>Don't have an account?</div>
-          <a href='https://www.instagram.com/accounts/emailsignup/' className='text-sm font-medium text-[#129cf6]'>Sign up</a>
-        </div>
-        <div className='text-[#8e8e8e] text-xs flex gap-3 flex-wrap items-center justify-center px-8 py-6'>
-          <div>Meta</div>
-          <div>About</div>
-          <div>Blog</div>
-          <div>Jobs</div>
-          <div>Help</div>
-          <div>API</div>
-          <div>Privacy</div>
-          <div>Terms</div>
-          <div>Top Accounts</div>
-          <div>Hashtags</div>
-          <div>Locations</div>
-          <div>Instagram Lite</div>
-          <div>Contact Uploading & Non-Users</div>
-        </div>
-
-        <div className='text-[#8e8e8e] text-xs flex gap-3 flex-wrap items-center justify-center px-6'>
-          <div>Dance</div>
-          <div>Food & Drink
-          </div>
-          <div>Home & Garden
-          </div>
-          <div>Music
-          </div>
-          <div>Visual Arts
-          </div>
-        </div>
-
-        <div className='text-[#8e8e8e] text-xs flex gap-3 flex-wrap items-center justify-center px-8 py-4'>
-          <div>
-            <select className='outline-none'><option value="en">English</option><option value="en-gb">English (UK)</option><option value="af">Afrikaans</option><option value="cs">Čeština</option><option value="da">Dansk</option><option value="de">Deutsch</option><option value="el">Ελληνικά</option><option value="es">Español (España)</option><option value="es-la">Español</option><option value="fi">Suomi</option><option value="fr">Français</option><option value="id">Bahasa Indonesia</option><option value="it">Italiano</option><option value="ja">日本語</option><option value="ko">한국어</option><option value="ms">Bahasa Melayu</option><option value="nb">Norsk</option><option value="nl">Nederlands</option><option value="pl">Polski</option><option value="pt-br">Português (Brasil)</option><option value="pt">Português (Portugal)</option><option value="ru">Русский</option><option value="sv">Svenska</option><option value="th">ภาษาไทย</option><option value="tl">Filipino</option><option value="tr">Türkçe</option><option value="zh-cn">中文(简体)</option><option value="zh-tw">中文(台灣)</option><option value="bn">বাংলা</option><option value="gu">ગુજરાતી</option><option value="hi">हिन्दी</option><option value="hr">Hrvatski</option><option value="hu">Magyar</option><option value="kn">ಕನ್ನಡ</option><option value="ml">മലയാളം</option><option value="mr">मराठी</option><option value="ne">नेपाली</option><option value="pa">ਪੰਜਾਬੀ</option><option value="si">සිංහල</option><option value="sk">Slovenčina</option><option value="ta">தமிழ்</option><option value="te">తెలుగు</option><option value="vi">Tiếng Việt</option><option value="zh-hk">中文(香港)</option><option value="bg">Български</option><option value="fr-ca">Français (Canada)</option><option value="ro">Română</option><option value="sr">Српски</option><option value="uk">Українська</option></select>
-
-          </div>
-          <div>© 2022 Instagram from Meta</div>
-        </div>
-      </main>
-
+      </div>
     </div>
   )
 }
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      fontFamily:{
-        ubuntu:['Ubuntu', 'sans-serif']
-      }
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
